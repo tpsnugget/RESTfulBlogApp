72dd591679726606f7ee1ae988a56d85b268adb1
Michael Giebner <mikegiebner@gmail.com>
2019-10-02 20:43:15 -0400

    # Version 1: Colt Steele's Web Dev Bootcamp, Section 31, Chapter 308 # RESTful Blog App: INDEX

---
+++ app.js
@@ -0,0 +1,39 @@
+var express       = require('express'),
+    app           = express(),
+    bodyParser    = require('body-parser')
+    mongoose      = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', {useNewUrlParser: true})
+
+var blogSchema = new mongoose.Schema({
+   title: String,
+   image: String,
+   body: String,
+   created: {type: Date, default: Date.now}
+})
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+var Blog = mongoose.model('Blog', blogSchema)
+
+//=================
+//    ROUTES
+//=================
+app.get('/', (req, res) => {
+   res.redirect('/blogs')
+})
+
+app.get('/blogs', (req, res) => {
+   Blog.find({}, (err, blogs) => {
+      if (err) {console.log(err)}
+      else {res.render('index', {blogs: blogs})}
+   })
+})
+//=================
+//    ROUTES
+//=================
+app.listen(3000, process.env.IP, () => {
+   console.log('The server is running...')
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Version 1: Colt Steele's Web Dev Bootcamp, Section 31, Chapter 308
+# RESTful Blog App: INDEX
+   
